c7f5ec944bb427d032b44c0eff1d6b06625a05d5
doomchit <wlsduf930@gamil.com>
2022-08-27 21:20:37 +0900

    create basic player

--- code/level.py
+++ code/level.py
@@ -1,5 +1,7 @@
 import pygame 
 from settings import *
+from player import Player
+
 
 class Level:
 	def __init__(self):
@@ -10,7 +12,13 @@ class Level:
 		# sprite groups
 		self.all_sprites = pygame.sprite.Group()
 
-	def run(self,dt):
+		self.setup()
+
+
+	def setup(self):
+		self.player = Player((640, 360), self.all_sprites)
+
+	def run(self, dt):
 		self.display_surface.fill('black')
 		self.all_sprites.draw(self.display_surface)
-		self.all_sprites.update()
+		self.all_sprites.update(dt)
--- code/main.py
+++ code/main.py
@@ -1,11 +1,13 @@
-import pygame, sys
+import sys
+import pygame
 from settings import *
 from level import Level
+
 
 class Game:
 	def __init__(self):
 		pygame.init()
-		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 		pygame.display.set_caption('Sprout land')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
@@ -16,10 +18,11 @@ class Game:
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-  
+
 			dt = self.clock.tick() / 1000
 			self.level.run(dt)
 			pygame.display.update()
+
 
 if __name__ == '__main__':
 	game = Game()
